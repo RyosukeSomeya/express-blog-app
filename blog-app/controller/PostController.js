5aab878724dea107be3713f7335d2c1382e842b6
RyosukeSomeya <rx.78.0618@gmail.com>
2021-09-30 01:00:08 +0900

    post投稿機能実装,バリデーション実装

--- blog-app/controller/PostController.js
+++ blog-app/controller/PostController.js
@@ -1,7 +1,9 @@
 const views = '../views/post/';
 const isLoggedIn = require('../services/isLoggedIn');
+const { validationResult } = require('express-validator');
 const User = require('../models').User;
 const Post = require('../models').Post;
+
 module.exports = {
   newPost: (req, res, next) => {
     const user = isLoggedIn(req.cookies.token);
@@ -21,7 +23,47 @@ module.exports = {
     }
   },
   createPost: (req, res, next) => {
-
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      let messages = [];
+      errors.errors.forEach((error) => {
+        messages.push(error.msg);
+      });
+      const user = isLoggedIn(req.cookies.token);
+      const data = {
+        isLoggedIn: true,
+        pageTitle: '新規投稿',
+        formMethod: 'POST',
+        actionPath: '/createpost',
+        userData: user,
+        btnText: '投稿',
+        messages: messages
+      };
+      res.render(views + 'new.ejs', data);
+    } else {
+      Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        userId: req.body.userid,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }).then(post => {
+        console.log('投稿完了')
+        res.redirect('/home');
+      }).catch(error => {
+        const user = isLoggedIn(req.cookies.token);
+        const data = {
+          isLoggedIn: true,
+          pageTitle: '新規投稿',
+          formMethod: 'POST',
+          actionPath: '/createpost',
+          userData: user,
+          btnText: '投稿',
+          messages: error.messages
+        };
+        res.render(views + 'new.ejs', data);
+      });
+    }
   },
   editPost: (req, res, next) => {
     const user = isLoggedIn(req.cookies.token);
--- blog-app/routes/post.route.js
+++ blog-app/routes/post.route.js
@@ -1,8 +1,9 @@
 const router = require('./auth.route')
 const postController = require('../controller/PostController');
+const postValidator = require('../middleware/postValidator');
 
 router.get('/newpost', postController.newPost);
-router.post('/createpost', postController.createPost);
+router.post('/createpost', postValidator, postController.createPost);
 // 記事IDをパラメータとして取るようにする
 router.get('/editpost', postController.editPost);
 router.put('/updatepost', postController.updatePost);
